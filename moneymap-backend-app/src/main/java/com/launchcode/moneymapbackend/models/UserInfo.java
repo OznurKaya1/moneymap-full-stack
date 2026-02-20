808fb5830a9e8bbc57133c0b9ac58323a64a8800
OznurKaya1 <oznurkaya1459@gmail.com>
2026-02-20 15:58:57 -0600

    Creating user info model

---
+++ moneymap-backend-app/src/main/java/com/launchcode/moneymapbackend/models/UserInfo.java
@@ -0,0 +1,78 @@
+package com.launchcode.moneymapbackend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String roles = "USER";
+
+    public UserInfo() {}
+
+    public UserInfo(String firstName, String lastName, String email, String password, String roles) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+
+
+    }
+}
