2b6ef054394cd7e1dd3538277c7f3a161fc7c1cd
OznurKaya1 <oznurkaya1459@gmail.com>
2026-03-10 15:21:19 -0500

    Add ExpenseService

---
+++ moneymap-backend-app/src/main/java/com/launchcode/moneymapbackend/service/ExpenseService.java
@@ -0,0 +1,42 @@
+package com.launchcode.moneymapbackend.service;
+
+import com.launchcode.moneymapbackend.models.Expenses;
+import com.launchcode.moneymapbackend.models.UserInfo;
+import com.launchcode.moneymapbackend.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class ExpenseService {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    public Expenses addExpense(UserInfo user, Expenses expense) {
+        expense.setUser(user);
+        return expenseRepository.save(expense);
+    }
+
+    public List<Expenses> getUserExpenses(UserInfo user) {
+        return expenseRepository.findByUser(user);
+    }
+
+    public List<Expenses> getExpensesByCategory(UserInfo user, String category) {
+        return expenseRepository.findByUserAndCategory(user, category);
+    }
+
+    public List<Expenses> getExpensesByDateRange(UserInfo user, LocalDate start, LocalDate end) {
+        return expenseRepository.findByUserAndDateBetween(user, start, end);
+    }
+
+    public Expenses updateExpense(Expenses expense) {
+        return expenseRepository.save(expense);
+    }
+
+    public void deleteExpense(Long id) {
+        expenseRepository.deleteById(id);
+    }
+}
