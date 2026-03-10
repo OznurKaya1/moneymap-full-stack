7860e7497a282494dd0d3d970664b9ecd5f85bd7
OznurKaya1 <oznurkaya1459@gmail.com>
2026-03-10 14:51:59 -0500

    creating expense repository

--- moneymap-backend-app/src/main/java/com/launchcode/moneymapbackend/repository/ExpenseRepository.java
+++ moneymap-backend-app/src/main/java/com/launchcode/moneymapbackend/repository/ExpenseRepository.java
@@ -1,13 +1,22 @@
 package com.launchcode.moneymapbackend.repository;
 
+import com.launchcode.moneymapbackend.models.Expenses;
 import com.launchcode.moneymapbackend.models.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expenses, Long> {
-    List<Expenses> findByUser(UserInfo user);
-    List<Expenses>findByUserAndCategory(UserInfo user, String category);
-    List<Expenses>findByUserAndDateBetween(UserInfo user, LocalDate start, LocalDate end);
 
+    // Find all expenses by a specific user
+    List<Expenses> findByUser(UserInfo user);
+
+    // Find all expenses by user and category
+    List<Expenses> findByUserAndCategory(UserInfo user, String category);
+
+    // Find all expenses by user within a date range
+    List<Expenses> findByUserAndDateBetween(UserInfo user, LocalDate start, LocalDate end);
 }
