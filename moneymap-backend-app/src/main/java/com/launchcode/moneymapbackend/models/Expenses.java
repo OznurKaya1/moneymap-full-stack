824ab93e78bf774e694d34f90a8a87151adcff0a
OznurKaya1 <oznurkaya1459@gmail.com>
2026-03-10 12:42:44 -0500

    Add Expense model

---
+++ moneymap-backend-app/src/main/java/com/launchcode/moneymapbackend/models/Expenses.java
@@ -0,0 +1,67 @@
+package com.launchcode.moneymapbackend.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Expenses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    private double amount;
+    private String desciription;
+    private String category;
+
+    @ManyToOne
+    private UserInfo user; // Many expenses can belong to one user
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDesciription() {
+        return desciription;
+    }
+
+    public void setDesciription(String desciription) {
+        this.desciription = desciription;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public UserInfo getUser() {
+        return user;
+    }
+
+    public void setUser(UserInfo user) {
+        this.user = user;
+    }
+}
