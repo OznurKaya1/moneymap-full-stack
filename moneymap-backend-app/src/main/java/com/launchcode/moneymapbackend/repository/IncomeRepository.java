c8fe0ab2982f9bcbdd051c8c82c2d21b5ac32832
OznurKaya1 <oznurkaya1459@gmail.com>
2026-03-10 14:56:08 -0500

    Creating income repository

---
+++ moneymap-backend-app/src/main/java/com/launchcode/moneymapbackend/repository/IncomeRepository.java
@@ -0,0 +1,15 @@
+package com.launchcode.moneymapbackend.repository;
+
+import com.launchcode.moneymapbackend.models.Income;
+import com.launchcode.moneymapbackend.models.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+    List<Income> findByUser(UserInfo user);
+    List<Income> findByUserAndDateBetween(UserInfo user, LocalDate start, LocalDate end);
+}
